d39f6b88c87c91cebcba2e718f19205beb3b77fc
4P5 <theprismarinescientist@gmail.com>
2021-09-16 10:22:43 +1200

    Initial commit
    Initial code. Currently just a system to read messages and send them, sans authentication.

---
+++ src/main/java/nz/fourp/msg/msgctrl/mixin/MessageMixin.java
@@ -0,0 +1,34 @@
+package nz.fourp.msg.msgctrl.mixin;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.network.packet.s2c.play.GameMessageS2CPacket;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.TranslatableText;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.text.MessageFormat;
+import java.util.Objects;
+
+
+@Mixin(ClientPlayNetworkHandler.class)
+public class MessageMixin {
+    @Inject(at = @At("HEAD"), method = "onGameMessage(Lnet/minecraft/network/packet/s2c/play/GameMessageS2CPacket;)V")
+    private void onGameMessage(GameMessageS2CPacket packet, CallbackInfo info) {
+        var message = packet.getMessage();
+        if (!(message instanceof TranslatableText)) {
+            return;
+        }
+        var translatable = ((TranslatableText) message);
+        if (!Objects.equals(translatable.getKey(), "commands.message.display.incoming")) {
+            return;
+        }
+        var privateMessageArgs = translatable.getArgs();
+        var sender = ((LiteralText) privateMessageArgs[0]).getString();
+        var messageText = ((LiteralText) privateMessageArgs[1]).getString();
+        System.out.println(MessageFormat.format("Private message received from {0}: {1}", sender, messageText));
+        MinecraftClient.getInstance().player.sendChatMessage(MessageFormat.format("{0}", messageText));
+    }
+}
